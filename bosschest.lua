284f044809270e960e3c5a31db78740816c85da8
SonNg20 <nguyentrungson2007.cx26@gmail.com>
2026-06-15 21:24:52 +0700

    Update bosschest.lua

--- bosschest.lua
+++ bosschest.lua
@@ -406,7 +406,7 @@ task.spawn(function()
                             pcall(function()
                                 hrp.CFrame = CFrame.new(spot + Vector3.new(0, 5, 0))
                             end)
-                            task.wait(9)
+                            task.wait(10)
                         end
                     end
                     lastCornerCheck = tick()
