a6788526624d995478c8ecc9e539cd919f408a6e
SonNg20 <nguyentrungson2007.cx26@gmail.com>
2026-06-17 01:00:14 +0700

    Update bosschest.lua

--- bosschest.lua
+++ bosschest.lua
@@ -20,7 +20,7 @@ if target then
 else
     warn("Không tìm thấy SideJoinEventTarget")
 end
-wait(20)
+wait(30)
 local container = workspace.__THINGS.__INSTANCE_CONTAINER.Active.Backrooms.GeneratedBackrooms
 local spawnRoom = container:FindFirstChild("SpawnRoom")
 local player = game:GetService("Players").LocalPlayer
